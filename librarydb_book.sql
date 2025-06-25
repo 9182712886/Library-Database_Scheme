a8470b35d14224194fca1c83307056d1b1da2d61
Rangisetti Bhaskar <127117549+9182712886@users.noreply.github.com>
2025-06-25 16:47:10 +0530

    Update librarydb_book.sql

--- librarydb_book.sql
+++ librarydb_book.sql
@@ -26,7 +26,7 @@ CREATE TABLE `book` (
   `book_id` int NOT NULL,
   `tittle` varchar(45) DEFAULT NULL,
   `published_year` int DEFAULT NULL,
-  `genre` varchar(45) DEFAULT NULL,
+  `available` boolean DEFAULT NULL,
   PRIMARY KEY (`book_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
